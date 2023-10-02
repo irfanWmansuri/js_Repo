eac80a121515a48b626770ce13742d3ca1a55e90
Irfan Mansuri <132755889+irfanWmansuri@users.noreply.github.com>
2023-10-02 07:06:59 +0000

    var let const exp.

--- 01_basic/text.js
+++ 01_basic/text.js
@@ -1,1 +1,9 @@
-console.log("irfan")
+const ID = 144221;
+let email = "irfan@gmail.com";
+var password = "112233"
+city = "palghar"
+// ID = 144222  --cannot redeclared or reassigned value.
+email = "irfan@gmail"
+// let email = "irfan@gmail"  --cannot redeclared but reassigned the value.
+
+console.table([ID,email,password,city])
